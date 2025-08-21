6ddce043141ed38eeaf3e3212dcabc6f3d2fe254
Steen hjalmar larsen <sthl@youmail.dk>
2025-08-21 14:58:49 +0200

    opgave 1 løst

--- arrays.js
+++ arrays.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
+    //opgave 1: Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle de tal som er større end 20.
+    let res1 = numbersArray.filter((elm) => elm > 20)
+    console.log(res1);
 
 }) // ends DOMContentLoaded
+
---
+++ opgaver.md
@@ -0,0 +1,14 @@
+# Opgaver
+
+## Opgave 1
+Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle de tal som er større end 20.
+
+## Udskriv det nye array i konsollen:
+
+``` js 
+//let newNumbers = //din løsning her...
+console.log("Opgave 1", newNumbers)
+//Så mister du ikke overblikket, mens du løser opgaverne.
+```
+
+## Opgave 2
